edbf6813c12ee06c28c2e7eb01dd09400c72e846
Bao Pham <BariumOxide@BaoPham.local>
2016-03-07 16:10:19 -0500

    added comments and close command

--- tenCharacterLimit.py
+++ tenCharacterLimit.py
@@ -1,29 +1,34 @@
 import os
 
-inFile = open( "input.txt", 'r' )
+inFile = open( "input.txt", 'r' ) # opens the input file in read mode
 
-if os.path.isfile ( "output.txt" ):
+if os.path.isfile ( "output.txt" ): # if output file already exists, opens it in read/write mode,
+# otherwise creates it by opening it in write mode
 
-	outFile = open( "output.txt", 'r+' )
+	outFile = open( "output.txt", 'r+' ) # don't open in 'w' if don't have to because
+	# 'w' is dangerous!	
 		
 else:
 
 	outFile = open( "output.txt", 'w' )
 
-originalMessage = inFile.readlines()
+originalMessage = inFile.readlines() # creates a list of lines by reading from the input file
 
-numberMessageLines = len( originalMessage )
+numberMessageLines = len( originalMessage ) # sets a variable equal the number of lines
 
 lineNumber = 0
 
-while numberMessageLines > lineNumber:
+while numberMessageLines > lineNumber: # program will stop reading after last line of message
 	
-	singleLine = originalMessage[ lineNumber ]
+	singleLine = originalMessage[ lineNumber ] # selects the current line
 	
-	if len( singleLine ) <= 10: 
+	if len( singleLine ) <= 10: # deletes the "\n" if line was short enough to fit it
 		
 		singleLine = singleLine[ : -1 ]
 	
-	outFile.write( singleLine [0:10] + "\n" )
+	outFile.write( singleLine [0:10] + "\n" ) # writes characters 0-10 of line + "\n"
 	
-	lineNumber += 1
+	lineNumber += 1 # adds to lineNumber to work towards terminating loop
+
+inFile.close() # Always close files -dont want data to get corrupted
+outFile.close()
